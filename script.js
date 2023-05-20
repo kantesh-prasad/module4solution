52a2742aea123989b8243d8a500c748438b8f7ac
kantesh-prasad <85825541+kantesh-prasad@users.noreply.github.com>
2023-05-20 23:46:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,17 +29,16 @@ WARNING!!! WARNING!!!
 
 
 (function () {
-
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
-    for (var i = 0; i < names.length; i++) {
+for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
     
-      if (firstLetter === 'j') {
-        byeSpeaker(names[i]);
+ if (firstLetter === 'j') {
+   byeSpeaker(names[i]);
       }
-      else {
-        helloSpeaker(names[i]);
+  else {
+    helloSpeaker(names[i]);
       }
     }
     
